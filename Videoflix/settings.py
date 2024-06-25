109899bc3e34cd96615558c313dd92b66c8d47e1
ChristianWestrich <christian.westrich@gmx.de>
2024-06-25 10:40:14 +0200

    new structure

--- Videoflix/settings.py
+++ Videoflix/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +76,9 @@ TEMPLATES = [
         },
     },
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'Videoflix.wsgi.application'
 
