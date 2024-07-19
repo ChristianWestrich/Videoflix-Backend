f5f5262305db534b4362edaae75928fd29c17e21
ChristianWestrich <christian.westrich@gmx.de>
2024-07-19 19:29:11 +0200

    update user model

--- users/models.py
+++ users/models.py
@@ -7,8 +7,9 @@ class CustomUser(AbstractUser, PermissionsMixin):
     first_name = models.CharField(max_length=255)
 
 
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.email
