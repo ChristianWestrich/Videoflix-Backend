53bc108b340f34f2c0e46ae04b918ca0d3604b59
ChristianWestrich <christian.westrich@gmx.de>
2024-07-24 12:11:27 +0200

    update email password reset an password confirm

--- content/signals.py
+++ content/signals.py
@@ -1,20 +1,14 @@
 import os
 from django.dispatch import receiver
-from Videoflix.settings import MEDIA_ROOT
 from .models import Movie
 from django.db.models.signals import post_save, post_delete
-
 import django_rq
 
-
-@receiver(post_save, sender= Movie)
+@receiver(post_save, sender=Movie)
 def video_post_save(sender, instance, created, **kwargs):
     if created and instance.video_file:
         queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue('content.tasks.convert_and_update_480p', instance.id, instance.video_file.path)
-        queue.enqueue('content.tasks.convert_and_update_720p', instance.id, instance.video_file.path)
-        queue.enqueue('content.tasks.convert_and_update_1080p', instance.id, instance.video_file.path)
-
+        queue.enqueue('content.tasks.convert_and_update', instance.id, instance.video_file.path)
 
 @receiver(post_delete, sender=Movie)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
@@ -24,15 +18,8 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
         file_root, file_ext = os.path.splitext(instance.video_file.path)
-        converted_480p = f"{file_root}_480p.mp4"
-        converted_720p = f"{file_root}_720p.mp4"
-        converted_1080p = f"{file_root}_1080p.mp4"
+        converted_files = [f"{file_root}_original.mp4", f"{file_root}_480p.mp4", f"{file_root}_720p.mp4", f"{file_root}_1080p.mp4"]
 
-        if os.path.isfile(converted_480p):
-            os.remove(converted_480p)
-
-        if os.path.isfile(converted_720p):
-            os.remove(converted_720p)
-
-        if os.path.isfile(converted_1080p):
-            os.remove(converted_1080p)
+        for file in converted_files:
+            if os.path.isfile(file):
+                os.remove(file)
--- content/tasks.py
+++ content/tasks.py
@@ -1,18 +1,21 @@
-import os
 import subprocess
 
 from Videoflix.settings import MEDIA_ROOT
 from content.models import Movie
 
-ffmpeg_path = '/opt/homebrew/bin/ffmpeg' #your path for ffmpeg on mac
-ffmpeg_path_windows = 'c:/usr/ffmpeg/bin/ffmpeg.exe'
+ffmpeg_path = 'c:/usr/ffmpeg/bin/ffmpeg.exe'  # Your path for ffmpeg on Windows
+
+def convert_original(source):
+    new_file_name = source.rsplit('.', 1)[0] + '_original.mp4'
+    cmd = f'"{ffmpeg_path}" -i "{source}" -c:v libx264 -crf 23 -c:a aac -strict -2 "{new_file_name}"'
+    subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return new_file_name
 
 def convert_480p(source):
     new_file_name = source.rsplit('.', 1)[0] + '_480p.mp4'
     cmd = f'"{ffmpeg_path}" -i "{source}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{new_file_name}"'
-    process = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return new_file_name
-
 
 def convert_720p(source):
     new_file_name = source.rsplit('.', 1)[0] + '_720p.mp4'
@@ -20,30 +23,41 @@ def convert_720p(source):
     subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return new_file_name
 
-
 def convert_1080p(source):
     new_file_name = source.rsplit('.', 1)[0] + '_1080p.mp4'
     cmd = f'"{ffmpeg_path}" -i "{source}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{new_file_name}"'
     subprocess.run(cmd, shell=True, capture_output=True, text=True)
     return new_file_name
 
+def convert_to_hls(source, resolution):
+    new_file_name = source.rsplit('.', 1)[0] + f'_{resolution}.m3u8'
+    cmd = (
+        f'"{ffmpeg_path}" -i "{source}" -c:v libx264 -crf 23 -c:a aac -strict -2 '
+        f'-start_number 0 -hls_time 10 -hls_list_size 0 -f hls "{new_file_name}"'
+    )
+    subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return new_file_name
 
-def convert_and_update_480p(instance_id, source):
-    new_file_name = convert_480p(source)
+def convert_and_update(instance_id, source):
+    resolutions = ['original', '480p', '720p', '1080p']
+    hls_files = {}
+
+    for res in resolutions:
+        if res == 'original':
+            new_file_name = convert_original(source)
+        elif res == '480p':
+            new_file_name = convert_480p(source)
+        elif res == '720p':
+            new_file_name = convert_720p(source)
+        elif res == '1080p':
+            new_file_name = convert_1080p(source)
+
+        hls_file = convert_to_hls(new_file_name, res)
+        hls_files[res] = hls_file.replace(MEDIA_ROOT, '').replace('\\', '/')
+
     movie_instance = Movie.objects.get(id=instance_id)
-    movie_instance.video_480p = new_file_name.replace(MEDIA_ROOT, '').replace('\\', '/')
-    movie_instance.save()
-
-
-def convert_and_update_720p(instance_id, source):
-    new_file_name = convert_720p(source)
-    movie_instance = Movie.objects.get(id=instance_id)
-    movie_instance.video_720p = new_file_name.replace(MEDIA_ROOT, '').replace('\\', '/')
-    movie_instance.save()
-
-
-def convert_and_update_1080p(instance_id, source):
-    new_file_name = convert_1080p(source)
-    movie_instance = Movie.objects.get(id=instance_id)
-    movie_instance.video_1080p = new_file_name.replace(MEDIA_ROOT, '').replace('\\', '/')
+    movie_instance.video_file = hls_files['original']
+    movie_instance.video_480p = hls_files['480p']
+    movie_instance.video_720p = hls_files['720p']
+    movie_instance.video_1080p = hls_files['1080p']
     movie_instance.save()
