678e88bdd7cb3cf94029244a3ca8e7c16833ee14
ChristianWestrich <christian.westrich@gmx.de>
2024-07-25 08:04:32 +0200

    update movieview and categories

--- content/signals.py
+++ content/signals.py
@@ -1,25 +1,27 @@
 import os
 from django.dispatch import receiver
-from .models import Movie
 from django.db.models.signals import post_save, post_delete
+from django.conf import settings
+from .models import Movie
 import django_rq
+from content.tasks import convert_and_update, delete_original_file
 
 @receiver(post_save, sender=Movie)
 def video_post_save(sender, instance, created, **kwargs):
     if created and instance.video_file:
         queue = django_rq.get_queue('default', autocommit=True)
-        queue.enqueue('content.tasks.convert_and_update', instance.id, instance.video_file.path)
+        queue.enqueue(convert_and_update, instance.id, instance.video_file.path, '480p')
+        queue.enqueue(convert_and_update, instance.id, instance.video_file.path, '720p')
+        queue.enqueue(convert_and_update, instance.id, instance.video_file.path, '1080p')
 
 @receiver(post_delete, sender=Movie)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
     if instance.thumbnail and os.path.isfile(instance.thumbnail.path):
         os.remove(instance.thumbnail.path)
-    if instance.video_file:
-        if os.path.isfile(instance.video_file.path):
-            os.remove(instance.video_file.path)
-        file_root, file_ext = os.path.splitext(instance.video_file.path)
-        converted_files = [f"{file_root}_original.mp4", f"{file_root}_480p.mp4", f"{file_root}_720p.mp4", f"{file_root}_1080p.mp4"]
+    for resolution in ['480p', '720p', '1080p']:
+        video_attr = getattr(instance, f'video_{resolution}', None)
+        if video_attr:
+            video_path = os.path.join(settings.MEDIA_ROOT, video_attr.strip('/'))
+            if os.path.isfile(video_path):
+                os.remove(video_path)
 
-        for file in converted_files:
-            if os.path.isfile(file):
-                os.remove(file)
