bbc180e8208f9e34aaebf31d4626adc997f1d68e
ChristianWestrich <christian.westrich@gmx.de>
2024-07-25 09:55:20 +0200

    install postgresql for database

--- Videoflix/settings.py
+++ Videoflix/settings.py
@@ -127,10 +127,21 @@ WSGI_APPLICATION = 'Videoflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflix',
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
